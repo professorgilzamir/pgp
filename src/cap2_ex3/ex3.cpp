0f61248c4ec7bc44fd781dd92b005276f737c35e
gilzamirgomes <gilzamir@gmail.com>
2017-11-20 22:52:30 -0300

    Shader encpasulation was added

--- src/cap2_ex3/ex3.cpp
+++ src/cap2_ex3/ex3.cpp
@@ -4,8 +4,8 @@
 #include <fstream>
 #include <string>
 #include <iostream>
-/* Usando o GLUT com gerenciador de janelas */
 
+/* Usando o GLUT com gerenciador de janelas */
 #ifdef __APPLE__
 #include <GLUT/glut.h>
 #else
@@ -13,7 +13,11 @@
 #include <GL/glut.h>
 #endif
 
+#include "shader.h"
+
 using namespace std;
+
+Shader *shader;
 
 GLfloat triangle_vertices[] = {
 	0.0, 0.8,
@@ -21,19 +25,17 @@ GLfloat triangle_vertices[] = {
 	0.8, -0.8
 };
 
-GLuint program;
-GLint attribute_coord2d;
 int mainwindow=-1;
 
-string* readfile(const char *name){
+string readfile(const char *name){
 	ifstream source(name);
 	string line;
-	string *content = new string();
+	string content;
 	if (source.is_open()){
 		while (!source.eof()) {
 			getline(source, line);
-			content->append(line);
-			content->append("\n");
+			content.append(line);
+			content.append("\n");
 		}
 		source.close();
 	}
@@ -41,85 +43,39 @@ string* readfile(const char *name){
 }
 
 /* COLOCAREMOS AS VARIAVEIS GLOBAIS AQUI MAIS TARDE */
-
 int inicializar(void)
 {
-	GLint compile_ok = GL_FALSE, link_ok = GL_FALSE;
-	GLuint vs = glCreateShader(GL_VERTEX_SHADER);
-	string *shadervs = readfile("shader.vs");
-	const char* vscontent = shadervs->data();
-	glShaderSource(vs, 1, &vscontent, NULL);
-	glCompileShader(vs);
-	glGetShaderiv(vs, GL_COMPILE_STATUS, &compile_ok);
-
-	if (0 == compile_ok){
-		delete shadervs;
-		fprintf(stderr, "Error in vertex shader\n");
+	string vs = readfile("shader.vs");
+	string fs = readfile("shader.fs");
+	try{
+		shader = new Shader(vs, fs);
+		shader->setAttribute(Attribute("coord2d"));
+		shader->setUniform(Uniform("color"));
+		shader->init();
+		glUseProgram(shader->getProgramID());
+		shader->setAttributeData("coord2d", triangle_vertices, sizeof(triangle_vertices));
+		shader->bindUData3f("color", 1.0f, 1.0f, 0.0f);
+		return 1;
+	} catch(string e) {
+		cout<<e<<endl;
 		return 0;
 	}
-
-	GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
-	string *shaderfs = readfile("shader.fs");
-	const char *fscontent = shaderfs->data();
-	glShaderSource(fs, 1, &fscontent, NULL);
-	glCompileShader(fs);
-	glGetShaderiv(fs, GL_COMPILE_STATUS, &compile_ok);
-	if (0==compile_ok) {
-		delete shadervs;
-		delete shaderfs;
-		fprintf(stderr, "Error in fragment shader\n");
-		return 0;
-	}
-
-	program = glCreateProgram();
-	glAttachShader(program, vs);
-	glAttachShader(program, fs);
-	glLinkProgram(program);
-	glGetProgramiv(program, GL_LINK_STATUS, &link_ok);
-
-	delete shadervs;
-	delete shaderfs;
-
-	if (!link_ok) {
-		fprintf(stderr, "Error in glLinkProgram");
-		delete shadervs;
-		delete shaderfs;		
-		return 0;
-	}
-
-
-	const char *attribute_name = "coord2d";
-	attribute_coord2d = glGetAttribLocation(program, attribute_name);
-	if (attribute_coord2d == -1) {
-		fprintf(stderr, "Could not bind attribute %s\n", attribute_name);
-		return 0;
-	}
-
-	return 1;
 }
 
 void atualizarDesenho()
 {
 	glClearColor(1.0, 1.0, 1.0, 1.0);
 	glClear(GL_COLOR_BUFFER_BIT);
-	glUseProgram(program);
-	glEnableVertexAttribArray(attribute_coord2d);
-	glVertexAttribPointer(attribute_coord2d,
-				2,
-				GL_FLOAT,
-				GL_FALSE,
-				0,
-				triangle_vertices
-				);
-
-	glDrawArrays(GL_LINE_LOOP, 0, 3);
-	glDisableVertexAttribArray(attribute_coord2d);
+	glUseProgram(shader->getProgramID());
+	shader->enableAttribute("coord2d");
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+	shader->disableAttribute("coord2d");
 	glutSwapBuffers();
 }
 
 void finalizar()
 {
-  glDeleteProgram(program);
+	delete shader;
 }
 
 void fecharJanela() {
