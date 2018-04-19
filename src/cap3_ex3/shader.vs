dd8245504ccb658e57ac428d07b05fe55f43a5d2
gilzamir <gilzamir@gmail.com>
2018-04-19 00:05:39 -0300

    Adicionado exemplo cap2_ex3: implementacao de iluminacao ambiente + difusa com sombreamento de Goraud

---
+++ src/cap3_ex3/shader.vs
@@ -0,0 +1,23 @@
+#version 120
+attribute vec3 coord3d;
+attribute vec3 normal3d;
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+varying vec4 finalColor;
+uniform vec4 materialColor;
+
+
+void  main(void) {
+
+	vec4 posFinal = model * vec4(coord3d, 1.0);
+
+	vec3 lightPos = vec3(0, 30, -10);
+	vec4 lightColor = vec4(0.8, 0.8, 0.8, 1.0);
+	vec3 L = normalize(lightPos-vec3(posFinal));
+	vec3 N = normalize(normal3d);
+
+	finalColor = vec4(0.4, 0.4, 0.4, 1.0) * materialColor + materialColor * lightColor * max(0, dot(N,L));
+
+	gl_Position =  projection * posFinal;
+}
---
+++ src/cap3_ex3/utils.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+
+namespace utils {
+
+	void getNormalFromVertex(GLuint vertexIndex, GLuint *faceIndex, GLuint *normalsIndex, GLfloat *normals,
+								GLuint numberOfFaces, GLuint numVerticesInFace,  GLfloat out[3]){
+		out[0] = 0.0f;
+		out[1] = 0.0f;
+		out[2] = 0.0f;
+
+
+		for (int i = 0; i < numberOfFaces; i++) {
+			GLuint pos = numVerticesInFace * i;
+			for (int j = 0; j < numVerticesInFace; j++) {
+				GLuint idx = faceIndex[pos+j];
+				if (idx == vertexIndex) {
+					GLuint nidx = i * 3;
+					out[0] += normals[nidx];
+					out[1] += normals[nidx+1];
+					out[2] += normals[nidx+2];
+				}
+			}
+		}
+	}
+
+
+	void getNormals(GLuint numberOfVertices, GLuint *faceIndex, GLuint *normalsIndex, GLfloat *normals,
+								GLuint numberOfFaces, GLuint numVerticesInFace, GLfloat *out) {
+		for (int i = 0; i < numberOfVertices; i++) {
+			GLfloat n[3];
+			getNormalFromVertex(i, faceIndex, normalsIndex, normals, numberOfFaces, numVerticesInFace, n);
+			int idx = i * 3;
+			out[idx] = n[0];
+			out[idx+1] = n[1];
+			out[idx+2] = n[2];
+		}
+	}
+}
