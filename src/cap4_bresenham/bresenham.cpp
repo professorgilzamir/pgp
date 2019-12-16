8e58dcf87aa780bd8b87a3bd78fffbc26b9fea35
gilzamir <gilzamir@gmail.com>
2019-12-16 12:21:00 -0300

    bug fix

--- src/cap4_bresenham/bresenham.cpp
+++ src/cap4_bresenham/bresenham.cpp
@@ -33,11 +33,11 @@ void show() {
 
 //INICIO DAS FUNCOES DE MAPEAMENTO ENTRE COORDENADAS DO UNIVERSO E COORDENADAS DA TELA
 int toXScr(float x, float minXSRU, float maxXSRU){
-	return  (int)(x * width/(maxXSRU-minXSRU) + 0.5f);
+	return  (int)( (MX * (x-minXSRU))/(maxXSRU-minXSRU) + 0.5f );
 }
 
 int toYScr(float y, float minYSRU, float maxYSRU) {
-	return (int)(y * (-height)/(maxYSRU-minYSRU) + 0.5f);
+	return (int)( MY * (y-minYSRU)/(maxYSRU-minYSRU) + 0.5f);
 }
 
 float toXSRU(int x, float minXSRU, float maxXSRU) {
@@ -52,10 +52,11 @@ float toYSRU(int y, float minYSRU, float maxYSRU) {
 //FIM DAS FUNCOES DE MAPEAMENTO ENTRE COORDENADAS DO UNIVERSO E COORDENADAS DA TELA
 
 void __bresenham(float x1, float y1, float x2, float y2, bool inv1 = false){
-	int sx1 = toXScr(x1, 0, 10);
-	int sy1 = toYScr(y1, 0, 10);
-	int sx2 = toXScr(x2, 0, 10);
-	int sy2 = toXScr(y2, 0, 10);
+	int sx1 = toXScr(x1, -10, 10);
+	int sy1 = toYScr(y1, -10, 10);
+	int sx2 = toXScr(x2, -10, 10);
+	int sy2 = toYScr(y2, -10, 10);
+	cout<<"Draw line from ("<<sx1<<", "<<sy1<<") to ("<<sx2<<", "<<sy2<<")"<<endl;
 	int dx = sx2 - sx1;
 	int dy = sy2 - sy1;
 	int p = 2 * dy - dx;
@@ -64,9 +65,9 @@ void __bresenham(float x1, float y1, float x2, float y2, bool inv1 = false){
 	int signaly = 1;
 	for (int x = sx1; x <= sx2; x += signalx) {
 		if (!inv1){
-			screen[y][x] = '*';
+			screen[MY-y][x] = '*';
 		} else {
-			screen[x][y] = '*';
+			screen[x][MY-y] = '*';
 		}
 		if ( p < 0 ) {
 			p = p + 2 * dy;
@@ -78,9 +79,11 @@ void __bresenham(float x1, float y1, float x2, float y2, bool inv1 = false){
 }
 
 
-
 int main(int argc, char *argv[]) {
-	show();
-	bresenham(10.0f, 10.0f, 0.0f, 0.0f);
+	//show();
+	__bresenham(-10, -10, 10, 10);
+	for (int i = 10; i>=-10; i--){
+		__bresenham(9, i, 10, i);
+	}
 	show();
 }
